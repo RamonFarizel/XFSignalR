814d872852a52e3ed33d77c25dca80127e6b57dd
Ramon Farizel <ramonfarizel@gmail.com>
2020-07-31 18:59:18 -0300

    Configuring API

---
+++ XFSignalRApi/Data/ChatContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using XFSignalRApi.Models;
+
+namespace XFSignalRApi.Data
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions<ChatContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Message> Message { get; set; }
+
+    }
+}
--- XFSignalRApi/Startup.cs
+++ XFSignalRApi/Startup.cs
@@ -5,20 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using XFSignalRApi.Data;
 using XFSignalRApi.Hubs;
 
 namespace XFSignalRApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSignalR();
+
+            services.AddDbContext<ChatContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ChatContext")));
         }
+    
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -32,11 +46,6 @@ namespace XFSignalRApi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-
                 endpoints.MapHub<ChatHub>("/chathub");
             });
         }
