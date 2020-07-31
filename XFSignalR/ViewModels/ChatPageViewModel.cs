ba7805d5284b4d82bc088317d2e15c41ac85130b
Ramon Farizel <ramonfarizel@gmail.com>
2020-07-31 17:58:02 -0300

    Adding Message model

--- XFSignalR/ViewModels/ChatPageViewModel.cs
+++ XFSignalR/ViewModels/ChatPageViewModel.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 namespace XFSignalR.ViewModels
 {
     public class ChatPageViewModel
     {
         public string Title { get; set; }
+        public string Message { get; set; }
+
+        public ICommand SendCommand { get; set; }
 
         public ChatPageViewModel()
         {
             Title = "ChatPage";
+            SendCommand = new Command(ExecuteSendCommand);
+        }
+
+        void ExecuteSendCommand()
+        {
         }
     }
 }
---
+++ XFSignalRApi/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace XFSignalRApi.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Autor { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+
+        public string Text { get; set; }
+    }
+}
